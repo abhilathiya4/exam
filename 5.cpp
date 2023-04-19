63d5c252e903cd2262aee4c25f81417efb343e80
abhilathiya4 <123169803+abhilathiya4@users.noreply.github.com>
2023-04-19 15:24:50 +0530

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class Math{
+
+	public : 
+		int a;
+		void set(int n){
+			a = n;
+		}
+		void get(){
+			cout<<a<<"\t"<<endl	;	
+		}
+		Math operator ++(){
+			Math temp;
+			temp.a = ++a;
+			return temp;
+		}
+		Math operator --(){
+			Math temp;
+			temp.a = --a;
+			return temp;
+		}
+		
+};
+
+int main(){
+ 	Math obj1,obj2,obj3;
+ 	
+ 	obj1.set(5);
+ 	obj1.get();
+ 	
+ 	obj2 = ++obj1;
+ 	obj2.get();
+ 	
+ 	obj3 = --obj1;
+ 	obj3.get();
+	
+	return 0;
+}
